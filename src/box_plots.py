50b119f0f3fb6cba62e8ccecef85fb3138a06e90
Charles Beam <charleslbeam@gmail.com>
2024-12-03 07:39:19 -0600

    added units to box plots

--- src/box_plots.py
+++ src/box_plots.py
@@ -3,17 +3,30 @@ import matplotlib.pyplot as plt
 import os
 
 # Load the dataset
-data = pd.read_csv('../data/Crop_recommendation.csv')
+data = pd.read_csv('data/Crop_recommendation.csv')
 print("Dataset Preview:")
 print(data.head())
 
 # columns = N,P,K,temperature,humidity,ph,rainfall,label
+# units = mg/kg, mg/kg, mg/kg, °C, %, ph, mm, crop
+
 # Create the plots directory if it doesn't exist
 if not os.path.exists('./plots'):
     os.makedirs('./plots')
 
-
 label_means = data.groupby('label').mean()
+
+print(label_means)
+
+units = {
+    'N': 'mg/kg',
+    'P': 'mg/kg',
+    'K': 'mg/kg',
+    'temperature': '°C',
+    'humidity': '%',
+    'ph': 'ph',
+    'rainfall': 'mm'
+}
 
 for column in data.columns[:-1]:
     plt.figure(figsize=(12, 8))
@@ -24,7 +37,7 @@ for column in data.columns[:-1]:
     plt.title(f'{column} vs crop')
     plt.suptitle('')
     plt.xlabel('crop')
-    plt.ylabel(column)
+    plt.ylabel(f'{column} ({units[column]})')
     plt.xticks(rotation=90)
     plt.tight_layout()
     plt.savefig(f'./plots/{column}_vs_crop.png', bbox_inches='tight', dpi=300)
